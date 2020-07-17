c0c3b885540fd5ae77163422b686d5eb5433cea8
AdithiyaG <adithiyagopal0912@gmail.com>
2020-07-18 00:05:45 +0530

    Assignment_2(AboutUS Page)

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -49,6 +50,7 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId}></Route>
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />}  />
               <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
