a11121f0eaad182f46835acfed49a80d4af1388b
AdithiyaG <adithiyagopal0912@gmail.com>
2020-07-17 22:26:17 +0530

    Single_Page_Application(created ContactUs,Clickable MenuItem,Home Pages)

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle,Badge} from 'reactstrap';
+
+function RenderCard({item}) {
+
+    return(
+        <Card>
+            <CardImg src={item.image} alt={item.name}  />
+            <CardBody>
+            <CardTitle><h4>{item.name}</h4></CardTitle>
+            <CardSubtitle>{item.label ? <Badge className='m-1 px-3 py-0' color='danger'><h5>{item.label}</h5></Badge> : null }{item.price ? <Badge className='m-1 py-0 px-3' pill><h6>&#36;{item.price}</h6></Badge> : null }</CardSubtitle>
+            {item.designation ? <CardSubtitle><h5>{item.designation}</h5></CardSubtitle> : null }
+            <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+
+}
 
 function Home(props) {
     return(
-      <div className="container">
-        <h4>Home</h4>
-      </div>
+        <div className="container">
+            <div className="row align-items-strech">
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.dish} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.leader} />
+                </div>
+            </div>
+        </div>
     );
 }
 
-export default Home;   
+export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import {Switch,Route,Redirect} from 'react-router-dom';
 class Main extends Component {
   constructor(props) {
@@ -12,7 +16,10 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      selectedDish:null
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+      
     };
   }
 
@@ -20,9 +27,20 @@ class Main extends Component {
   {
     const HomePage = () => {
       return(
-          <Home/>
+        <Home 
+        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+    />
       );
     }
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+    
 
     return (
       <div >
@@ -30,6 +48,8 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId}></Route>
+              <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
         
